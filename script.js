f1e06d230688050d5ae0a95d3b5b0eb4b0388504
Leocaliban <leocaliban@gmail.com>
2019-03-30 01:56:41 -0300

    Teste de registro

---
+++ script.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+    window.onload = function () {
+        navigator.serviceWorker.register('/b7web-service-workers/sw.js')
+            .then(function () {
+                console.log('ServiceWorker registrado!');
+            }, function (e) {
+                console.log('Ocorreu um erro no registro do ServiceWorker: ');
+                console.log(e);
+            });
+    };
+}
